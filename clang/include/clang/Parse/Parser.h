e923564c4b972af8c93d791a831609d89f28bc62
Christopher Di Bella <cjdb@google.com>
2022-08-22 00:27:10 +0000

    [clang] adds unary type transformations as compiler built-ins
    Adds
    
    * `__add_lvalue_reference`
    * `__add_pointer`
    * `__add_rvalue_reference`
    * `__decay`
    * `__make_signed`
    * `__make_unsigned`
    * `__remove_all_extents`
    * `__remove_extent`
    * `__remove_const`
    * `__remove_volatile`
    * `__remove_cv`
    * `__remove_pointer`
    * `__remove_reference`
    * `__remove_cvref`
    
    These are all compiler built-in equivalents of the unary type traits
    found in [[meta.trans]][1]. The compiler already has all of the
    information it needs to answer these transformations, so we can skip
    needing to make partial specialisations in standard library
    implementations (we already do this for a lot of the query traits). This
    will hopefully improve compile times, as we won't need use as much
    memory in such a base part of the standard library.
    
    [1]: http://wg21.link/meta.trans
    
    Co-authored-by: zoecarver
    
    Reviewed By: aaron.ballman, rsmith
    
    Differential Revision: https://reviews.llvm.org/D116203

--- clang/include/clang/Parse/Parser.h
+++ clang/include/clang/Parse/Parser.h
@@ -2907,7 +2907,6 @@ private:
   void AnnotateExistingDecltypeSpecifier(const DeclSpec &DS,
                                          SourceLocation StartLoc,
                                          SourceLocation EndLoc);
-  void ParseUnderlyingTypeSpecifier(DeclSpec &DS);
   void ParseAtomicSpecifier(DeclSpec &DS);
 
   ExprResult ParseAlignArgument(SourceLocation Start,
@@ -3005,6 +3004,8 @@ private:
          SourceLocation &EllipsisLoc);
   void ParseBracketDeclarator(Declarator &D);
   void ParseMisplacedBracketDeclarator(Declarator &D);
+  bool MaybeParseTypeTransformTypeSpecifier(DeclSpec &DS);
+  DeclSpec::TST TypeTransformTokToDeclSpec();
 
   //===--------------------------------------------------------------------===//
   // C++ 7: Declarations [dcl.dcl]
